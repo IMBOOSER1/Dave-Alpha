cad15c8c2882a64e2e17eb52f02ac5475c2eb124
IMBOOSER1 <44923714+IMBOOSER1@users.noreply.github.com>
2018-11-30 16:59:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -155,14 +155,14 @@ client.user.setGame(`Dave-Alpha`,"https://www.twitch.tv/settings/profile")
 
 client.on("message", async message => {
 
-  if(command === "ping") {
+  if(command === "?ping") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
   
-  if(command === "say") {
+  if(command === "?say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
@@ -172,7 +172,7 @@ client.on("message", async message => {
     message.channel.send(sayMessage);
   }
   
-  if(command === "kick") {
+  if(command === "?kick") {
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
     // Please read on Array.some() to understand this bit: 
     // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
@@ -200,7 +200,7 @@ client.on("message", async message => {
 
   }
   
-  if(command === "ban") {
+  if(command === "?ban") {
     // Most of this command is identical to kick, except that here we'll only let admins do it.
     // In the real world mods could ban too, but this is just an example, right? ;)
     if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
